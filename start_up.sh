ad3955b40540e7534904d62d76b91ba6881588de
Tomo Kaneko <tomo.kaneko@proton.me>
2023-05-25 19:16:52 -0700

    Add comments

--- start_up.sh
+++ start_up.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# make entering chroot easy
+# 1. mount physical drives/partitions
+# 2. mount virtual kernel file system
+# 3. enter chroot root=/mnt/lfs
+
 bash mount_partitions.sh
 bash mount_virtual_kernel_fs.sh
 bash enter_chroot.sh
