896a06b4102809ed78b6c718771b2ddd85582e4e
Tomo Kaneko <tomo.kaneko@proton.me>
2023-05-31 22:26:57 -0700

    remove firefox startup

--- start_up.sh
+++ start_up.sh
@@ -4,10 +4,7 @@
 # 1. mount physical drives/partitions
 # 2. mount virtual kernel file system
 # 3. enter chroot root=/mnt/lfs
-# 4. open build logs
 
 bash mount_partitions.sh
 bash mount_virtual_kernel_fs.sh
 bash enter_chroot.sh
-
-firefox https://www.linuxfromscratch.org/lfs/build-logs/11.3/
